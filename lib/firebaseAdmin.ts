5172029487245da1242f54f365db2dc0ed537575
neosteplabs <neosteplabs@gmail.com>
2026-03-08 03:50:51 -0500

    remove firebase env build crash

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -8,9 +8,6 @@ if (
   !process.env.FIREBASE_PROJECT_ID ||
   !process.env.FIREBASE_CLIENT_EMAIL ||
   !process.env.FIREBASE_PRIVATE_KEY
-) {
-  throw new Error("Missing Firebase Admin environment variables");
-}
 
 const firebaseAdminConfig = {
   credential: cert({
